382cce6894a23068e56e3d27277e35d507f844e8
Emerson <emersonnnb@hotmail.com>
2020-01-28 21:31:24 -0300

    tela de login do adm

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,124 +1,115 @@
 <?php session_start(); ?>
 <?php
-   if(!isset($_SESSION['login'])) header("location: login.php");  
+if (!isset($_SESSION['login'])) header("location: login.php");
 ?>
-
-
-
 <!DOCTYPE html>
 <html>
-  <head>
+
+<head>
 	<meta charset="utf-8">
-    <title>Acesso Restrito</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-   
+	<title>Acesso Restrito</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-    <link href="css/styles.css" rel="stylesheet">
 
-  </head>
-  <body>
-  	<div class="header">
-	     <div class="container">
-	        <div class="row">
-	           <div class="col-md-5">
-	              <!-- Logo -->
-	              <div class="logo">
-	                 <h1><a href="index.php">TecInfo</a></h1>
-	              </div>
-	           </div>
-	           <div class="col-md-5">
-	              <div class="row">
-	                <div class="col-lg-12">
-	                </div>
-	              </div>
-	           </div>
-	           <div class="col-md-2">
-	              <div class="navbar navbar-inverse" role="banner">
-	                  <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-	                    <ul class="nav navbar-nav">
-	                      <li class="dropdown">
-	                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $_SESSION['login']; ?> <b class="caret"></b></a>
-							
-	                        <ul class="dropdown-menu animated fadeInUp">
-	                          <li><a href="restrito-sair.php">Sair</a></li>
-	                        </ul>
-	                      </li>
-	                    </ul>
-	                  </nav>
-	              </div>
-	           </div>
-	        </div>
-	     </div>
+	<link href="css/styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+	<div class="header">
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Link</a>
+					</li>
+					<li class="nav-item dropdown">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+							Minha conta
+						</a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="#">Editar</a>
+							<div class="dropdown-divider"></div>							
+							<a class="dropdown-item" href="#">Sair</a>							
+						</div>
+					</li>
+					
+				</ul>				
+			</div>
+		</nav>
 	</div>
 
-    <div class="page-content">
-    	<div class="row">
-		  <div class="col-md-2">
-		  	<div class="sidebar content-box" style="display: block;">
-                <ul class="nav">
-                    <!-- Main menu -->
-                    <li class="current"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Início</a></li>
-					
-                    <li class="submenu">
-                         <a href="#">
-                            <i class="glyphicon glyphicon-user"></i>Acesso Restrito
-                            <span class="caret pull-right"></span>
-                         </a>
-                         <!-- Sub menu -->
-                         <ul>
-							<li><a href="restrito-cadastrar.php"><i class="icon-inbox"></i>Cadastrar Usuários administrativos</a></li>
-							<li><a href="restrito-lista.php"><i class="icon-inbox"></i>Lista de Usuários </a></li>
-							<li><a href="restrito-form-pesquisa-nome.php"><i class="icon-inbox"></i>Pesquisar Usuários </a></li>
-                        </ul>
-                    </li>  
-					
-					<li class="submenu">
-                         <a href="#">
-                      <i class="glyphicon glyphicon-user"></i>Cliente
-                            <span class="caret pull-right"></span>
-                         </a>
-                         <!-- Sub menu -->
-                         <ul>
-							<li><a href="form-cliente.php"><i class="icon-inbox"></i>Cadastrar Cliente </a></li>
-							<li><a href="cliente-lista.php">Clientes Cadastrados</a></li>
-							<li><a href="cliente-form-pesquisa-nome.php">Pesquisar cliente</a></li> 
-                        </ul>
-                    </li>
+	<div class="page-content">
+		<div class="row">
+			<div class="col-md-2">
+				<div class="sidebar content-box" style="display: block;">
+					<ul class="nav">
+						<!-- Main menu -->
+						<li class="current"><a href="index.php"><i class="glyphicon glyphicon-home"></i> Início</a></li>
+
 						<li class="submenu">
-                         <a href="#">
-                      <i class="glyphicon glyphicon-user"></i>Produto
-                            <span class="caret pull-right"></span>
-                         </a>
-                         <!-- Sub menu -->
-                         <ul>
-							<li><a href="form-produto.php"><i class="icon-inbox"></i>Cadastrar Produto </a></li>
-							<li><a href="produto-lista.php">Produtos Cadastrados</a></li>
-							<li><a href="produto-form-pesquisa-nome.php">Pesquisar Produto</a></li> 
-                        </ul>
-                    </li>
-					
-					<li class="submenu">
-                         <a href="#">
-                      <i class="glyphicon glyphicon-user"></i>Pedido
-                            <span class="caret pull-right"></span>
-                         </a>
-                         <!-- Sub menu -->
-                         <ul>
-							
-							<li><a href="pedido-lista.php">Pedidos Cadastrados</a></li>
-							<li><a href="pedido-pesquisaporcliente.php">Pedidos por cliente</a></li>
-							<li><a href="pedido-form-pesquisa-nome.php">Pesquisar Pedido</a></li> 
-                        </ul>
-                    </li>
-					
-					
-					
-					
-					 <li class="current"><a href="cliente-sair.php"><i class="glyphicon glyphicon-log-out"></i>Sair</a></li>		
-                </ul>
-             </div>
-		  </div>
+							<a href="#">
+								<i class="glyphicon glyphicon-user"></i>Acesso Restrito
+								<span class="caret pull-right"></span>
+							</a>
+							<!-- Sub menu -->
+							<ul>
+								<li><a href="restrito-cadastrar.php"><i class="icon-inbox"></i>Cadastrar Usuários administrativos</a></li>
+								<li><a href="restrito-lista.php"><i class="icon-inbox"></i>Lista de Usuários </a></li>
+								<li><a href="restrito-form-pesquisa-nome.php"><i class="icon-inbox"></i>Pesquisar Usuários </a></li>
+							</ul>
+						</li>
+
+						<li class="submenu">
+							<a href="#">
+								<i class="glyphicon glyphicon-user"></i>Cliente
+								<span class="caret pull-right"></span>
+							</a>
+							<!-- Sub menu -->
+							<ul>
+								<li><a href="form-cliente.php"><i class="icon-inbox"></i>Cadastrar Cliente </a></li>
+								<li><a href="cliente-lista.php">Clientes Cadastrados</a></li>
+								<li><a href="cliente-form-pesquisa-nome.php">Pesquisar cliente</a></li>
+							</ul>
+						</li>
+						<li class="submenu">
+							<a href="#">
+								<i class="glyphicon glyphicon-user"></i>Produto
+								<span class="caret pull-right"></span>
+							</a>
+							<!-- Sub menu -->
+							<ul>
+								<li><a href="form-produto.php"><i class="icon-inbox"></i>Cadastrar Produto </a></li>
+								<li><a href="produto-lista.php">Produtos Cadastrados</a></li>
+								<li><a href="produto-form-pesquisa-nome.php">Pesquisar Produto</a></li>
+							</ul>
+						</li>
+
+						<li class="submenu">
+							<a href="#">
+								<i class="glyphicon glyphicon-user"></i>Pedido
+								<span class="caret pull-right"></span>
+							</a>
+							<!-- Sub menu -->
+							<ul>
+
+								<li><a href="pedido-lista.php">Pedidos Cadastrados</a></li>
+								<li><a href="pedido-pesquisaporcliente.php">Pedidos por cliente</a></li>
+								<li><a href="pedido-form-pesquisa-nome.php">Pesquisar Pedido</a></li>
+							</ul>
+						</li>
+
+
+
+
+						<li class="current"><a href="cliente-sair.php"><i class="glyphicon glyphicon-log-out"></i>Sair</a></li>
+					</ul>
+				</div>
+			</div>
--- admin/login.php
+++ admin/login.php
@@ -1,68 +1,102 @@
 <!DOCTYPE html>
-<html>
-  <head>
-    <title>Acesso Restrito</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <link href="css/bootstrap.min.css" rel="stylesheet">
- 
-    <link href="css/styles.css" rel="stylesheet">
-
-  
-  </head>
-
-  
-  <body class="login-bg">
-  	<div class="header">
-	     <div class="container">
-	        <div class="row">
-	           <div class="col-md-12">
-	              <!-- Logo -->
-	              <div class="logo">
-	                 <h1><a href="login.php">RESTRITO</h1></a>
-	              </div>
-	           </div>
-	        </div>
-	     </div>
-	</div>
-
+<html lang="en">
+<head>
+	<title>Área Restrita</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
 	
-	<div class="page-content container">
-		<div class="row">
-			<div class="col-md-4 col-md-offset-4">
-				<div class="login-wrapper">
-			        <div class="box">
-			            <div class="content-wrap">
-							
-							<br/>
-							<br/>
-			                <h3>ACESSO RESTRITO</h3>
-			               <br/>
-				<form class="form-vertical" method="post" action="restrito-autenticar.php">			
-			                <input class="form-control" type="text" name="login" id="login"  placeholder="Usuário">
-			                <input class="form-control" type="password" name="senha" id="senha" placeholder="Senha">
-			                <div class="action">
-			                    <button type="submit" class="btn btn-primary">Entrar</button>
-			                    <button type="reset"  class="btn btn-danger">Limpar</button>
-								<a href="../index.php" class="btn btn-primary" value="Home">Home</a>
-			                </div>   
-						</form>
-								
-			            </div>
-			        </div>
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form class="login100-form validate-form" method="POST" action="restrito-autenticar.php">
+					<span class="login100-form-title p-b-33">
+						Acesso restrito
+					</span>
 
-			        
-			    </div>
+					<div class="wrap-input100 validate-input" data-validate = "Login obrigatório">
+						<input class="input100" type="text" name="login" placeholder="Login" autocomplete="off">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="wrap-input100 rs1 validate-input" data-validate="Senha obrigatória">
+						<input class="input100" type="password" name="senha" placeholder="Senha" autocomplete="off">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-20">
+						<button class="login100-form-btn">
+							Sign in
+						</button>
+					</div>
+
+					<div class="text-center p-t-45 p-b-4">
+						<span class="txt1">
+							Forgot
+						</span>
+
+						<a href="#" class="txt2 hov1">
+							Username / Password?
+						</a>
+					</div>
+
+					<div class="text-center">
+						<span class="txt1">
+							Create an account?
+						</span>
+
+						<a href="#" class="txt2 hov1">
+							Sign up
+						</a>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
+	
 
+	
+<!--===============================================================================================-->
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
 
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://code.jquery.com/jquery.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/custom.js"></script>
-  </body>
+</body>
 </html>
